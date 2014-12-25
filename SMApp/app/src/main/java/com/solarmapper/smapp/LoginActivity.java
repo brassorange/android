b5dd714a9f13ec5398f4c17db2b331b554f6ae44
Boris Georgiev <boris.georgiev@solarmapper.com>
2014-12-26 01:38:24 +0200

    Log in/out improvements.

--- SMApp/app/src/main/java/com/solarmapper/smapp/LoginActivity.java
+++ SMApp/app/src/main/java/com/solarmapper/smapp/LoginActivity.java
@@ -5,8 +5,8 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
-import android.content.ContentResolver;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -16,6 +16,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,6 +27,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.solarmapper.smapp.controllers.HttpRetriever;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,18 +37,11 @@ import java.util.List;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
+    Intent intent;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
@@ -58,6 +54,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setupActionBar();
+
+        intent = this.getIntent();
 
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
@@ -273,23 +271,21 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         protected Boolean doInBackground(Void... params) {
             // TODO: attempt authentication against a network service.
 
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+            // Simulate network access.
+            HttpRetriever httpRetriever = new HttpRetriever();
+            Log.d(this.getClass().getSimpleName(), "update from http ...");
+            String result = httpRetriever.retrieve("http://solarmapper.com/api/v1/login?uid=" + mEmail + "&pwd=" + mPassword);
+            Log.d(this.getClass().getSimpleName(), "result: " + result);
+            if (result != null && result != "") {
+                intent.putExtra("apiKey", result.replace("\"", ""));
+                return true;
+            } else {
+                intent.putExtra("apiKey", "");
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
             // TODO: register the new account here.
-            return true;
+//            return true;
         }
 
         @Override
@@ -298,8 +294,10 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             showProgress(false);
 
             if (success) {
+                setResult(RESULT_OK, intent);
                 finish();
             } else {
+                setResult(RESULT_CANCELED, intent);
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
             }
